81dfc57082d803618ef42cd1c08115a700790f12
Adejumo Adedayo <adejumoadedayo350@gmail.com>
2025-12-11 04:47:36 +0100

    remove the if __name__ == __main__: blocks. for easy deployment

--- app.py
+++ app.py
@@ -74,10 +74,3 @@ def index():
 
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
